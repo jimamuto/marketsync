06053060ee435db0c6145d04de2b2d51d4357c89
jim amuto <jim.amuto@strathmore.edu>
2026-06-19 18:17:48 +0300

    navbar: add more navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar (){
   return(
-  <header className="Navbar">
+  <header className="navbar">
       <Link href="/" className="navbar-logo">
            MarketSync
          </Link>
@@ -11,6 +11,9 @@ export default function Navbar (){
            <Link href="/">Home</Link>
            <Link href="/register">Register</Link>
            <Link href="/login">Login</Link>
+           <Link href="/farmer">Farmer</Link>
+           <Link href="/buyer">Buyer</Link>
+           <Link href="/admin">Admin</Link>
          </nav>
 
     </header>
