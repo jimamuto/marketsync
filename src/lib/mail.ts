af0901aecefe42f2dbbc45570d95317858180011
jim amuto <jim.amuto@strathmore.edu>
2026-06-21 22:07:20 +0300

    api: test email can be sent using the mailing helper

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,33 +1,32 @@
-import nodemmailer from "nodemailer";
-const stmpPort = Number(process.env.STMP_PORT?? 465);
+import nodemailer from "nodemailer";
+const smtpPort = Number(process.env.SMTP_PORT ?? 465);
 
-export const mailTransporter = nodemmailer.createTransport({
-  host:process.env.STMP_PORT,
-  port:stmpPort,
-  secure:process.env.STMP_SECURE =="TRUE",
-  auth:{
-    user:process.env.STMP_USER,
-    pass:process.env.STMP_PASS,
+export const mailTransporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: smtpPort,
+  secure: process.env.SMTP_SECURE === "true",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
-type sendMailOptions ={
-  to:string;
-  subject:string;
-  html:string;
+type sendMailOptions = {
+  to: string;
+  subject: string;
+  html: string;
 }
 
-export async function sendMail({to,subject,html}:sendMailOptions) {
-  if(!process.env.STMP_USER){
-    throw new Error("stmp user is not configured");
+export async function sendMail({ to, subject, html }: sendMailOptions) {
+  if (!process.env.SMTP_USER) {
+    throw new Error("SMTP user is not configured");
   }
 
   return mailTransporter.sendMail({
-  from: `"MarketSync" <${process.env.SMTP_USER}>`,
-       to,
-       subject,
-       html,
+    from: `"MarketSync" <${process.env.SMTP_USER}>`,
+    to,
+    subject,
+    html,
   });
-  
 }
 
